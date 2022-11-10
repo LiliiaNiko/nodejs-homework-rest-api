59e264142a5b0b2113e80dd0da0e322fdd853e7e
LiliiaNikolaichuk <l.nikolaychuk@ukr.net>
2022-11-10 19:07:09 +0200

    mongo-db

---
+++ controllers/contacts.controller.js
@@ -0,0 +1,79 @@
+const { Contact } = require("../models/contact.model");
+const { createNotFoundHttpError } = require("../helpers/index");
+
+async function getAll(req, res, next) {
+  const contacts = await Contact.find();
+  return res.json({
+    data: contacts,
+  });
+}
+
+async function create(req, res, next) {
+  const createdContact = await Contact.create(req.body);
+  return res.status(201).json({
+    data: {
+      contact: createdContact,
+    },
+  });
+}
+
+async function deleteById(req, res, next) {
+  const { id } = req.params;
+  const contact = await Contact.findById(id);
+  if (contact) {
+    await Contact.findByIdAndDelete(id);
+    return res.json({
+      data: { contact },
+    });
+  }
+  return next(createNotFoundHttpError());
+}
+
+async function updateById(req, res, next) {
+  const { id } = req.params;
+
+  const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  return res.json({
+    data: {
+      contact: updatedContact,
+    },
+  });
+}
+
+async function findOneById(req, res, next) {
+  const { id } = req.params;
+  const contact = await Contact.findById(id);
+  if (contact) {
+    return res.json({
+      data: { contact },
+    });
+  }
+  return next(createNotFoundHttpError());
+}
+
+async function updateStatusContact(req, res, next) {
+  const { id } = req.params;
+  const { favorite } = req.body;
+  if (!favorite) {
+    return res.status(400).json({ message: "missing field favorite" });
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  return res.json({
+    data: {
+      contact: updatedContact,
+    },
+  });
+}
+
+module.exports = {
+  getAll,
+  create,
+  deleteById,
+  updateById,
+  findOneById,
+  updateStatusContact,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,53 +1,17 @@
 const express = require("express");
+const contactController = require("../../controllers/contacts.controller");
+const { tryCatchWrapper } = require("../../helpers/index");
 
 const router = express.Router();
-const contacts = require("../../models/contacts");
 
-router.get("/", async (req, res, next) => {
-  const contactsList = await contacts.listContacts();
-  res.json(contactsList);
-  next();
-});
-
-router.get("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  const contact = await contacts.getContactById(id);
-  if (contact) {
-    return res.json(contact);
-  }
-  next();
-});
-
-router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  if (!name || !email || !phone) {
-    return res.status(400).json({ message: "missing required name field" });
-  }
-  const newContact = await contacts.addContact(name, email, phone);
-  return res.status(201).json(newContact);
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  if (id !== contacts.id) {
-    return res.status(404).json({ message: "Not found" });
-  }
-  const newContacts = await contacts.removeContact(id);
-  res.json(newContacts);
-
-  next();
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  if (!name || !email || !phone) {
-    return res.status(400).json({ message: "missing fields" });
-  }
-  const newContact = await contacts.addContact(name, email, phone);
-  if (newContact) {
-    return res.status(200).json(newContact);
-  }
-  return res.status(404).json({ message: "Not found" });
-});
+router.get("/", tryCatchWrapper(contactController.getAll));
+router.get("/:id", tryCatchWrapper(contactController.findOneById));
+router.post("/", tryCatchWrapper(contactController.create));
+router.delete("/:id", tryCatchWrapper(contactController.deleteById));
+router.put("/:id", tryCatchWrapper(contactController.updateById));
+router.patch(
+  "/:id/favorite",
+  tryCatchWrapper(contactController.updateStatusContact)
+);
 
 module.exports = router;
